7d518bfd556679a6d1c6b076893fa90c509edd9b
Bowrna <mailbowrna@gmail.com>
2021-10-02 13:02:45 +0530

    new features added - message and notification

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,8 @@ from langdetect import detect, LangDetectException
 from flask_babel import _, get_locale
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm, SearchForm
-from app.models import User, Post
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm, SearchForm, MessageForm
+from app.models import User, Post, Message
 from app.email import send_password_reset_email
 
 @app.route('/', methods=['GET','POST'])
@@ -205,3 +205,33 @@ def search():
     return render_template('search.html', title=_('Search'), posts=posts,
                            next_url=next_url, prev_url=prev_url)
 
+@app.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+    return render_template('user_popup.html', user=user, form=form)
+
+@app.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash('Your message has been sent')
+        return redirect(url_for('user', username=recipient))
+    return render_template('send_message.html', title='Send Message', form=form, recipient=recipient)
+
+@app.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for('messages', page=messages.prev_num) if messages.has_prev else None
+    return render_template('messages.html', messages=messages.items)
